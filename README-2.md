89c4e948f6b8e54c21fa4fac0eb4eaa9a109b02a
amyspencerproject <amy@spencerproject.com>
2022-07-08 08:11:07 -0600

    Initial commit for this CRUD to its own project folder

---
+++ README-2.md
@@ -0,0 +1,5 @@
+# Amy's Notes for this Tutorial
+
+First mistake was having this entire project inside of the tutorial folder. Moved it out an put under git revision control.
+
+## 
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  // return (
+  //   // ...
+  // );
+}
+export default App;
+
+
+
+// Everything below this line was originally in file. Not sure if I can delete it all or not.
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
